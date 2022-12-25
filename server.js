f62efe36d6476742d75715a15eb1d1c34c59b369
NamalSanjaya <namalsanjaya23@gmail.com>
2022-12-25 11:48:03 +0530

    Improve logs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ ws.on("connection", (sckt)=> {
     if(!sessionTb.IsTokenThere(token)){
         token = GenUniqueToken();
         username = username || GenUsername();
-        sckt.emit("add-token", {"token": token, "username": username});
+        sckt.emit("add-token", token, username);
     }
     sessionTb.Add(token, sckt.id, username);
     socketList.Add(sckt.id, sckt);
@@ -25,11 +25,11 @@ ws.on("connection", (sckt)=> {
     historyTb.GetAllHistory().then((results) => {
         sckt.emit("all-msgs/user", results);
     }).catch((reason) => console.error(reason));
-    console.log(`token : ${token}, socketId: ${sckt.id}, username: ${username}`);
+    console.log(`connected with token : ${token}, socketId: ${sckt.id}, username: ${username}`);
 
     // broadcast to all online users (expect the sender). auth={token, username}
     sckt.on("msg/broadcast", (username, msg) => {
-        if(username === "" || msg === "") {return}
+        if(username == "" || msg == "") {return}
         let list = socketList.GetSocketList();
         historyTb.InsertRow(username, msg).catch( (reason) => console.error(reason) );
         for (let onlineScktId in list){
@@ -42,17 +42,16 @@ ws.on("connection", (sckt)=> {
                 "owner_id" : username,
                 "content": msg
             });
-            console.log(`msg/broadcast : [${sckt.id}, ${username}] --> ${onlineScktId}`)
         }
-        console.log(`msg/broadcast done socketId: ${sckt.id}, username: ${username}`);
+        console.log(`msg/broadcast, socketId: ${sckt.id}, username: ${username}`);
     })
 
     // client disconnecting logic
     sckt.on("disconnect", (reason) => {
-        // remove socket from SocketList
         socketList.Delete(sckt.id);
         console.log(`socketId ${sckt.id} disconnect due to ${reason}`);
     })
+    // TODO: open not disconnected sockets are still there. find a way to remove those
 })
 
 console.log("listen 8000...");
